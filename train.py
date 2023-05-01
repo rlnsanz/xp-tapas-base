6e2997dc2efa2d7e830487c0f3523ca321ff0e83
Rolando Garcia <rogarcia@berkeley.edu>
2023-05-01 17:38:57 +0000

    adding wtq data

---
+++ train.py
@@ -0,0 +1,9 @@
+from .utils import parse_question
+from transformers import TapasConfig, TapasForQuestionAnswering, TapasTokenizer
+
+# or, the base sized model with WTQ configuration
+model_name = "google/tapas-base-finetuned-wtq"
+config = TapasConfig.from_pretrained(model_name)
+tokenizer = TapasTokenizer.from_pretrained(model_name)
+model = TapasForQuestionAnswering.from_pretrained("google/tapas-base", config=config)
+assert isinstance(model, TapasForQuestionAnswering)

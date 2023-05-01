4c73083afcb2ec7d8e036978c46b32a4f528e746
Rolando Garcia <rogarcia@berkeley.edu>
2023-05-01 21:31:34 +0000

    .

--- WikiTableQuestions-master/release-compact.sh
+++
@@ -1,4 +0,0 @@
-#/usr/bin/env bash
-# Release the compact version of the dataset
-cd ..
-zip -r WikiTableQuestions.zip WikiTableQuestions/csv WikiTableQuestions/data WikiTableQuestions/tagged WikiTableQuestions/evaluator.py WikiTableQuestions/README.md
--- train.py
+++ train.py
@@ -1,9 +1,15 @@
 from utils import parse_question
 from transformers import TapasConfig, TapasForQuestionAnswering, TapasTokenizer
 
+import torch
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # or, the base sized model with WTQ configuration
 model_name = "google/tapas-base-finetuned-wtq"
 config = TapasConfig.from_pretrained(model_name)
 tokenizer = TapasTokenizer.from_pretrained(model_name)
 model = TapasForQuestionAnswering.from_pretrained("google/tapas-base", config=config)
 assert isinstance(model, TapasForQuestionAnswering)
+model = model.to(device)
